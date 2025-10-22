1f406c9b21657267f6d510b2ef65324eba6f6511
Hubbi_Hassan15 <105710502+hassanjabir@users.noreply.github.com>
2025-10-23 02:06:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // A unique name for the cache. Using a version number helps in managing updates.
-const CACHE_NAME = 'vaxiliv-cache-v1.1';
+const CACHE_NAME = 'vaxiliv-cache-v1.2';
 
 // A list of all the essential files your app needs to work offline.
 // This is the "app shell".
@@ -105,4 +105,5 @@ self.addEventListener('fetch', event => {
           });
       })
   );
+
 });
